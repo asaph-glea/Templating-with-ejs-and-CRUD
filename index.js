8027f248a5a5eddd459efebd6bd7fa97e18c6b4a
asaph-glea <asaph.chege20@gmail.com>
2022-07-12 10:48:54 +0300

    first commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
+const Farm = require('./models/farm')
 const Product = require('./models/product');
-const { rmSync } = require('fs');
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
 .then(()=>{
@@ -21,46 +22,95 @@ app.set('view engine','ejs')
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'))
 
-const categories = ['fruit','vegetables','dairy','shroom']
-
-app.get('/products', async (req,res) =>{
-    const products = await Product.find({});
-    res.render('products/index',{ products })
+//Farm Routes
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms })
+})
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm })
 })
 
-app.get('/products/new',(req,res)=>{
-    res.render('products/new',{categories});
+app.delete('/farms/:id', async (req, res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+
+    res.redirect('/farms');
 })
 
-app.post('/products',async(req,res)=>{
-    const newProduct = new Product(req.body)
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`)
+})
+
+//Product Routes
+app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+})
+
+app.get('/products/new', async(req, res) => {
+    const farm = await Farm.find({categories,});
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
     await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`);
+    res.redirect(`/products/${newProduct._id}`)
 })
 
-app.get('/products/:id', async (req,res)=>{
+app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
-    res.render('products/show',{ product })
+    const product = await Product.findById(id).populate('farm', 'name');
+    res.render('products/show', { product })
 })
 
-app.get('/products/:id/edit',async (req,res)=>{
+app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit',{ product, categories })
+    res.render('products/edit', { product, categories })
 })
-app.delete('/products/:id',async(req,res)=>{
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
-    res.redirect('/products')
+    res.redirect('/products');
 })
-
-app.put('/products/:id', async (req,res)=>{
-    const { id } = req.params;
-    const product =await Product.findByIdAndUpdate(id,req.body,{runValidators:true, new:true});
-    res.redirect(`/products/${product._id}`)
-})
-
 app.listen(3000, ()=>{
     console.log("Sucess on port 3000");
 })
